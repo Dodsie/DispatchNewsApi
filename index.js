ed9d67797bef02387cc9785e197260a75ef2a9fa
Alastair <alastair05@gmail.com>
2022-05-18 16:33:27 -0700

    Add chat box and features

--- index.js
+++ index.js
@@ -5,6 +5,13 @@ const app = express();
 const port = 3001;
 const cors = require('cors');
 
+
+const http = require("http");
+const { Server } = require("socket.io");
+
+const server = http.createServer(app);
+
+
 app.use(cors());
 app.use(bodyParser.json());
 app.use(
@@ -12,6 +19,41 @@ app.use(
     extended: true,
   })
 );
+
+
+
+
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST"],
+  },
+});
+
+io.on("connection", (socket) => {
+  console.log(`User Connected: ${socket.id}`);
+
+  socket.on("join_room", (data) => {
+    socket.join(data);
+    console.log(`User with ID: ${socket.id} joined room: ${data}`);
+  });
+
+  socket.on("send_message", (data) => {
+    socket.to(data.room).emit("receive_message", data);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("User Disconnected", socket.id);
+  });
+});
+
+server.listen(3001, () => {
+  console.log("SERVER RUNNING");
+});
+
+
+
+
 
 
 app.get("/", (request, response) => {
