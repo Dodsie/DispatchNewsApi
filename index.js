ba01aa1539d8617679ddad6ff1e6e0affb6060f5
Alastair <alastair05@gmail.com>
2022-05-19 15:56:24 -0700

    Add chat server and socket

--- index.js
+++ index.js
@@ -1,54 +1,53 @@
 const express = require("express");
-const bodyParser = require("body-parser");
+// const bodyParser = require("body-parser");
 const db = require("./queries");
 const app = express();
 const port = 3001;
-const cors = require('cors');
+// const cors = require('cors');
+
 
 
 const http = require("http");
-const { Server } = require("socket.io");
+// const { Server } = require("socket.io");
 const server = http.createServer(app);
 
 
-app.use(cors());
-app.use(bodyParser.json());
-app.use(
-  bodyParser.urlencoded({
-    extended: true,
-  })
-);
+// app.use(cors());
+// app.use(bodyParser.json());
+// app.use(
+//   bodyParser.urlencoded({
+//     extended: true,
+//   })
+// );
 
 
+// const io = new Server(server, {
+//   cors: {
+//     origin: "http://localhost:3000",
+//     methods: ["GET", "POST"],
+//   },
+// });
 
+// io.on("connection", (socket) => {
+//   console.log(`User Connected: ${socket.id}`);
 
-const io = new Server(server, {
-  cors: {
-    origin: "http://localhost:3000",
-    methods: ["GET", "POST"],
-  },
-});
+//   socket.on("join_room", (data) => {
+//     socket.join(data);
+//     console.log(`User with ID: ${socket.id} joined room: ${data}`);
+//   });
 
-io.on("connection", (socket) => {
-  console.log(`User Connected: ${socket.id}`);
+//   socket.on("send_message", (data) => {
+//     socket.to(data.room).emit("receive_message", data);
+//   });
 
-  socket.on("join_room", (data) => {
-    socket.join(data);
-    console.log(`User with ID: ${socket.id} joined room: ${data}`);
-  });
+//   socket.on("disconnect", () => {
+//     console.log("User Disconnected", socket.id);
+//   });
+// });
 
-  socket.on("send_message", (data) => {
-    socket.to(data.room).emit("receive_message", data);
-  });
-
-  socket.on("disconnect", () => {
-    console.log("User Disconnected", socket.id);
-  });
-});
-
-server.listen(3001, () => {
-  console.log("SERVER RUNNING");
-});
+// server.listen(3001, () => {
+//   console.log("SERVER RUNNING");
+// });
 
 
 
